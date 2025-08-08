eec6e5a48ffa174eb22872393a852e074db017e7
James Snavely <jsnavely@macminijim.local>
2025-08-07 20:02:05 -0400

    autoformat

--- init.lua
+++ init.lua
@@ -206,5 +206,29 @@ cmp.setup.cmdline(':', {
 -- LSP setup
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-vim.lsp.enable('pyright')
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
+
 vim.lsp.enable('ruff')
+vim.lsp.enable('pyright')
+
+-- Auto-format Python files on save with ruff
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.py",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
